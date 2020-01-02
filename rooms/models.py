22bfdfd691da97d7e1aac536dfa7cf13aa6f0003
Bonggt <kepy1106@gmail.com>
2020-01-02 23:48:56 +0900

    #4.5_Meta Class & Photos Model

--- rooms/models.py
+++ rooms/models.py
@@ -21,28 +21,45 @@ class RoomType(AbstractItem):
 
     """ RoomType Model Definition """
 
-    pass
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ["name"]
 
 
 class Amenity(AbstractItem):
 
     """ Amenity Model Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Amenities"
 
 
 class Facility(AbstractItem):
 
     """ Facility Model Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Facilities"
 
 
 class HouseRule(AbstractItem):
 
     """ HouseRule Model Definition """
 
-    pass
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+    
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
@@ -65,9 +82,9 @@ class Room(core_models.TimeStampedModel):
     # Toknow - on_delete(CASCADE): 부모가 삭제되면, 자식도 삭제됨(일대다관계)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
     room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity)
-    facilities = models.ManyToManyField(Facility)
-    house_rule = models.ManyToManyField(HouseRule)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rule = models.ManyToManyField(HouseRule, blank=True)
 
     def __str__(self):
         return self.name
