844df97a7bb7004668fb3404e16707f851d6e80f
bonggt <kepy1106@gmail.com>
2020-01-11 23:30:17 +0900

    #8.6_ raw_ids and Inline Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -13,11 +13,20 @@ class ItemAdmin(admin.ModelAdmin):
     def used_by(self, obj):
         return obj.rooms.count()
 
+    pass
+
+
+class PhotoInline(admin.TabularInline):
+
+    model = models.Photo
+
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
+
+    inlines = (PhotoInline,)
 
     fieldsets = (
         (
@@ -65,6 +74,8 @@ class RoomAdmin(admin.ModelAdmin):
         "city",
         "country",
     )
+
+    raw_id_fields = ("host",)
 
     search_fields = ["city", "^host__username"]
 
