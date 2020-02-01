465bdd51cb39c6d698aae986f2c1fd705ffca841
Bonggt <kepy1106@gmail.com>
2020-02-01 21:11:08 +0900

    #11.6 Handling Exceptions (page method를 통해 특정 에러를 핸들링)

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
@@ -7,6 +7,10 @@ def all_rooms(request):
     request_page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=5)
-    room_page = paginator.page(int(request_page))
-    return render(request, "rooms/home.html", {"Page": room_page})
+    try:
+        room_page = paginator.page(int(request_page))
+        return render(request, "rooms/home.html", {"Page": room_page})
+    except EmptyPage:
+        room_page = paginator.page(1)
+        return redirect("/")
 
