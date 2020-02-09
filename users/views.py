cced87f8eb617b6a6a2d0924ea02f8c80e966fbb
Bonggt <kepy1106@gmail.com>
2020-02-09 17:51:39 +0900

    #14.5 UserLogin & Logout: Login and Logout(Easier way)

--- users/views.py
+++ users/views.py
@@ -1,26 +1,44 @@
-from django.views import View
-from django.shortcuts import render, redirect, reverse
+from django.views.generic import FormView
+from django.shortcuts import redirect, reverse
+from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
-class LoginView(View):
+class LoginView(FormView):
     """ LoginView Definition """
 
-    def get(self, request):
-        form = forms.LoginForm(initial={"email": "kepy1106@gmail.com"})
-        return render(request, "users/login.html", {"form": form})
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+    initial = {"email": "kepy1106@gmail.com"}
 
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(reverse("core:home"))
-        return render(request, "users/login.html", {"form": form})
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+# class LoginView(View):
+#     """ LoginView Definition """
+
+#     def get(self, request):
+#         form = forms.LoginForm(initial={"email": "kepy1106@gmail.com"})
+#         return render(request, "users/login.html", {"form": form})
+
+#     def post(self, request):
+#         form = forms.LoginForm(request.POST)
+#         if form.is_valid():
+#             email = form.cleaned_data.get("email")
+#             password = form.cleaned_data.get("password")
+#             user = authenticate(request, username=email, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect(reverse("core:home"))
+#         return render(request, "users/login.html", {"form": form})
 
 
 def log_out(request):
@@ -30,3 +48,6 @@ def log_out(request):
 
 # Login
 # https://docs.djangoproject.com/en/3.0/topics/auth/default/#how-to-log-a-user-in
+
+# LoginView
+# https://docs.djangoproject.com/en/3.0/topics/auth/default/#all-authentication-views
