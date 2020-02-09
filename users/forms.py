fe9c286c0323b4f604aa4c1f77c21c9b371cc178
Bonggt <kepy1106@gmail.com>
2020-02-09 16:43:14 +0900

    #14.2 UserLogin & Logout: Validating Email

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from . import models
 
 
 class LoginForm(forms.Form):
@@ -7,3 +8,16 @@ class LoginForm(forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    # https://docs.djangoproject.com/en/3.0/ref/forms/validation/#form-and-field-validation
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(username=email)
+            return email
+        except models.User.DoesNotExist:
+            raise forms.ValidationError("User does not exist")
+
+    def clean_password(self):
+        print("clean password")
+        return "password"
