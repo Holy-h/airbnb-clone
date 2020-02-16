71b503bbe5a5cc774cb0e7f13e845a159e4999ac
bonggt <kepy1106@gmail.com>
2020-02-16 10:39:13 +0900

    #17.3 Log in with Github: Recap(try/except)

--- users/views.py
+++ users/views.py
@@ -77,50 +77,52 @@ def github_login(request):
     )
 
 
+class GithubException(Exception):
+    """ GithubException Definition """
+
+    pass
+
+
 def github_callback(request):
-    client_id = os.environ.get("GITHUB_ID")
-    client_secret = os.environ.get("GITHUB_SECRET")
-    code = request.GET.get("code", None)
-    if code is not None:
-        result = requests.post(
-            f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
-            headers={"Accept": "application/json"},
-        )
-        result_json = result.json()
-        error = result_json.get("error", None)
-        if error is not None:
-            return redirect(reverse("users:login"))
-        else:
-            access_token = result_json.get("access_token")
-            profile_request = requests.get(
-                "https://api.github.com/user",
-                headers={
-                    "Authorization": f"token {access_token}",
-                    "Accept": "application/json",
-                },
+    try:
+        client_id = os.environ.get("GITHUB_ID")
+        client_secret = os.environ.get("GITHUB_SECRET")
+        code = request.GET.get("code", None)
+        if code is not None:
+            result = requests.post(
+                f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
+                headers={"Accept": "application/json"},
             )
-            # api 요청 성공 여부 파악 status_code or resultData
-            profile_json = profile_request.json()
-            username = profile_json.get("login", None)
-            if username is not None:
-                # 필요한 UserData 담기
-                name = profile_json.get("name")
-                email = profile_json.get("email")
-                bio = profile_json.get("bio")
-                # 기존에 가입한 유저가 있는지 확인
-                user = models.User.objects.get(email=email)
-                if user is not None:
-                    return redirect(reverse("users:login"))
-                else:
-                    user = models.User.objects.create(
-                        username=email, first_name=name, bio=bio, email=email
-                    )
-                    login(request, user)
-                    return redirect(reverse("core:home"))
+            result_json = result.json()
+            error = result_json.get("error", None)
+            if error is not None:
+                raise GithubException()
             else:
-                return redirect(reverse("users:login"))
-    else:
-        return redirect(reverse("core:home"))
+                access_token = result_json.get("access_token")
+                profile_request = requests.get(
+                    "https://api.github.com/user",
+                    headers={
+                        "Authorization": f"token {access_token}",
+                        "Accept": "application/json",
+                    },
+                )
+                # api 요청 성공 여부 파악 status_code or resultData
+                profile_json = profile_request.json()
+                username = profile_json.get("login", None)
+                if username is not None:
+                    # 필요한 UserData 담기
+                    name = profile_json.get("name")
+                    email = profile_json.get("email")
+                    bio = profile_json.get("bio")
+                    # 기존에 가입한 유저가 있는지 확인
+                    user = models.User.objects.get(email=email)
+
+                else:
+                    raise GithubException()
+        else:
+            raise GithubException()
+    except GithubException:
+        return redirect(reverse("users:login"))
 
 
 # Login
