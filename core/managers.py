deff8d708eb5b86343320847bcd231ecb75845ad
Bonggt <kepy1106@gmail.com>
2020-03-21 20:38:12 +0900

    #25.10 Message: Form and craete

--- core/managers.py
+++ core/managers.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import UserManager
 
 
 class CustomModelManager(models.Manager):
+
+    """ CustomModelManager Definition """
+
     def get_or_none(self, **kwargs):
         try:
             return self.get(**kwargs)
         except self.model.DoesNotExist:
             return None
+
+
+class CustomUserManager(CustomModelManager, UserManager):
+    pass
