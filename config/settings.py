b9f6cf03c8e001eda2305b593d15f3d2e0166913
Bonggt <kepy1106@gmail.com>
2020-03-24 20:19:59 +0900

    settings.py DJANGO_SECRET & ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "0x#zhf%e-v4!ejf3qzg=pxgx%o4p=1j*4atm9@bu_-9*2t0i+x"
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".elasticbeanstalk.com", "my-site.com"]
 
 
 # Application definition
