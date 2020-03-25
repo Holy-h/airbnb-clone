c7133a2499f6ac7e6b3027fd6ba54e121e991637
Bonggt <kepy1106@gmail.com>
2020-03-25 19:46:14 +0900

    #26.8 Staticfiles

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,8 @@
+import os
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
-
-def trigger_error(request):
-    divition_by_zero = 1 / 0
 
 
 urlpatterns = [
@@ -16,8 +13,7 @@ urlpatterns = [
     path("reviews/", include("reviews.urls", namespace="reviews")),
     path("lists/", include("lists.urls", namespace="lists")),
     path("conversations/", include("conversations.urls", namespace="conversations")),
-    path("admin/", admin.site.urls),
-    path("sentry-debug/", trigger_error),
+    path(os.environ.get("DJANGO_ADMIN_URL", "admin/"), admin.site.urls),
 ]
 
 if settings.DEBUG:
