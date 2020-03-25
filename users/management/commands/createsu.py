eef72bca7ff23133be66f0e689e193510f2a56da
Bonggt <kepy1106@gmail.com>
2020-03-25 15:49:53 +0900

    26.7 Superuser

---
+++ users/management/commands/createsu.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from django_seed import Seed
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    help = "This command creates superuser"
+
+    def handle(self, *args, **kwargs):
+
+        admin = User.objects.get_or_none(username="ebadmin")
+        if not admin:
+
+            User.objects.create_superuser("ebadmin", "kepy1106@gmail.com", "123123")
+            self.stdout.write(self.style.SUCCESS(f"Superuser Created"))
+
+        else:
+
+            self.stdout.write(self.style.SUCCESS(f"Superuser Exists"))
